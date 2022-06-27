77621e89a62a308448e35860ae83d8b69637ed68
shantanu-joshi21 <74046031+shantanu-joshi21@users.noreply.github.com>
2022-06-27 11:50:20 +0530

    Update Util.java

--- app/src/main/java/com/example/bda/Email/Util.java
+++ app/src/main/java/com/example/bda/Email/Util.java
@@ -2,5 +2,5 @@ package com.example.bda.Email;
 
 public class Util {
     public static final String EMAIL="myblooddonationapp21@gmail.com";
-    public static final String PASSWORD="";
+    public static final String PASSWORD="azwspqjjodfcvtuo";
 }
